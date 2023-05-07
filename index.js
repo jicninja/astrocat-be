12011e074d9c753565329981072a39d8b4265a59
jicninja <castroignacio888@gmail.com>
2023-05-07 00:55:00 -0300

    imporve server position and player list cache

--- index.js
+++ index.js
@@ -1,38 +1,64 @@
-const express = require("express");
-const { createServer } = require("http");
+const express = require('express');
+const { createServer, get } = require('http');
+const { addPlayer, getOtherPlayers, getPlayer, editPlayer, deletePlayer } = require('./modules/playerCache');
 const path = require('path');
-
-const port = process.env.PORT || 3000;
 
 const app = express();
 const server = createServer(app);
 
-app.use(express.static('public'));
+const port = process.env.PORT || 3000;
 
+/* STATIC SERVER - Landing Page */
+app.use(express.static('public'));
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
+/* RealTime Server */
 const io = require('socket.io')(server, {
   cors: {
     origin: '*'
   }    
 });
 
+/* Authentication */
 io.use((socket, next) => {
-    if (socket.handshake.query.token === "UNITY") {
+    if (socket.handshake.query.token === 'UNITY') {
         next();
     } else {
-        next(new Error("Authentication error"));
+        next(new Error('Authentication error'));
     }
 });
 
+/* On Connect */
 io.on('connection', socket => {
-    socket.emit('newUser', { PlayerId: socket.id });
+  const { id } = socket;
 
-    socket.on('userMove', (data) => {
-      socket.emit('userMove',{ PlayerId: socket.id }, data);
-    });
+  /* Add player cache */
+  addPlayer(id, { position : 0 });
+
+  /* Emit existing user */
+  const otherUsers = getOtherPlayers(id);
+  if(otherUsers.length) {
+    socket.emit('existingUsers', otherUsers);
+  }
+  
+  /*  Broadcast new user */
+  socket.broadcast.emit('newUser', socket.id);
+    socket.on('moveUser', data => { 
+      const currentPlayer = getPlayer(id);
+      if(currentPlayer.position != data) {
+        editPlayer(id, { position: data });  
+          // console.log('id: ', socket.id, ' pos:',  data);
+          socket.broadcast.emit('moveUser', id, currentPlayer.position);
+      }
+  });
+
+
+  socket.on('disconnect', () => {    
+    deletePlayer(id);
+    socket.broadcast.emit('removeUser', { id });
+  });
 
 });
 
---
+++ modules/playerCache.js
@@ -0,0 +1,37 @@
+const players = new Map();
+
+const addPlayer = (id, data) => players.set(id, data);
+const addArrayPlayer = (playerArray) => {
+    playerArray.map(({id , ...rest}) => {
+        players.set(id, rest);
+    });
+};
+
+const editPlayer = (id, data) => {
+    if(players.get(id)) {
+        players.set(id, data);
+    }    
+};
+
+const getPlayersCount = () => players.size;
+
+const getPlayers = (isArray) => {
+    if(isArray) {
+        const array = [];
+        players.forEach((value, key) => {
+            array.push({id: key, value});            
+        });
+        return array;
+    }
+    return players;
+};
+
+const getOtherPlayers = (currentId) => {
+    return getPlayers(true).filter(({id})=> id != currentId);
+}
+
+const deletePlayer = (id) => players.delete(id);
+
+const getPlayer = (id) => players.get(id);
+
+module.exports = { getPlayers, addPlayer, deletePlayer, editPlayer, getPlayersCount, getPlayer, getOtherPlayers, addArrayPlayer };
